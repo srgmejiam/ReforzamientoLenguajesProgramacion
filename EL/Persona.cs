73b61134997ec2cb8c314986c4ce72ec31b0b32c
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-03-09 09:35:20 -0600

    Agregar archivos de proyecto.

---
+++ EL/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    public class Persona
+    {
+        //Atributos
+        //Indicador de Acceso - tipo dato - Nombre - metodos Get and Set
+        public int IdPersona { get; set; }
+        public string Name { get; set; }
+        public string Genero { get; set; }
+        public int Edad { get; set; }
+
+        public static void Caminar()
+        {
+            Console.WriteLine("Caminando");
+        }
+        public static string Saludar(string Nombre)
+        {
+            return "Hola " + Nombre;
+        }
+    }
+}
---
+++ ReforzamientoLenguajesProgramacion/Program.cs
@@ -0,0 +1,23 @@
+﻿using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Reforzamineto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instancia de la clase Persona
+            EL.Persona Persona = new EL.Persona();
+            Persona.IdPersona = 1;
+            Persona.Name = "Luis";
+            Persona.Genero = "Masculino";
+            Persona.Edad = 20;
+        }
+    }
+}
